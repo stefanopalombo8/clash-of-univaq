040fac1840a21a6e57dcb6a2e199601c48b7e80c
stefanopalombo8 <stefano.palombo10@gmail.com>
2023-09-19 18:42:38 +0200

    primo commit sulla configurazione di run e prima vista (non definitiva)

---
+++ src/main/java/it/univaq/disim/oop/myclashofunivaq/MyClashOfUnivaqApplication.java
@@ -0,0 +1,27 @@
+package it.univaq.disim.oop.myclashofunivaq;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MyClashOfUnivaqApplication extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		//TEST
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/viste/homepage.fxml"));
+		Parent login = loader.load();
+		Scene scene = new Scene(login);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+
+}
