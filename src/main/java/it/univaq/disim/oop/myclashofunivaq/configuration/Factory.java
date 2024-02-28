dbd887692bfc6e32a7196bcf73aa5184247a3de0
stefanopalombo8 <stefano.palombo10@gmail.com>
2024-02-28 22:15:16 +0100

    generalizzazione della factory di carte e non più di soli personaggi

--- src/main/java/it/univaq/disim/oop/myclashofunivaq/configuration/Factory.java
+++ src/main/java/it/univaq/disim/oop/myclashofunivaq/configuration/Factory.java
@@ -1,22 +1,21 @@
 package it.univaq.disim.oop.myclashofunivaq.configuration;
 
 import java.io.FileInputStream;
-
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
-import java.util.HashSet;
 
+import it.univaq.disim.oop.myclashofunivaq.business.impl.MosseSpeciali;
+import it.univaq.disim.oop.myclashofunivaq.domain.Carta;
 import it.univaq.disim.oop.myclashofunivaq.domain.MossaSpeciale;
 import it.univaq.disim.oop.myclashofunivaq.domain.Personaggio;
 import it.univaq.disim.oop.myclashofunivaq.domain.Tank;
 import it.univaq.disim.oop.myclashofunivaq.domain.nomipersonaggi.TankNomi;
 
-public class Factory implements PersonaggioFactory {
+public class Factory implements CartaFactory {
 	
 	private static final String cartellaConfigurazione = "/configurations/";
 	private static final String puntoSeparatore = "."; //nel caso nel file si vuole cambiare il separatore
@@ -27,21 +26,32 @@ public class Factory implements PersonaggioFactory {
 		return instance;
 	}
 	
-	@Override
-	public Set<Personaggio> findAllPersonaggi() {
-		return ImplementazionePersonaggi.getPersonaggi();
+	private String costruisciPath(String fileName) { 
+		String pathCompleto = getClass().getResource(cartellaConfigurazione + fileName).toExternalForm();
+		return pathCompleto.substring(pathCompleto.indexOf("C"));
+	}
+	
+	private <T extends Carta> String costruisciChiave(T carta) {
+		StringBuilder builder = new StringBuilder();
+		builder.append(instance.ricercaCategoriaEimpostaNome(carta));
+		builder.append(puntoSeparatore);
+		builder.append(carta.getNome());
+		builder.append(puntoSeparatore);
+		
+		return builder.toString();
 	}
 	
 	@Override
-	public <T extends Personaggio> void modellaPersonaggio(T personaggioEmpty) {
-		if(personaggioEmpty == null)
+	public <T extends Carta> void modellaCarta (T carta) {
+		if(carta == null)
 			throw new IllegalArgumentException();
 	
-		String keyDaCercare = costruisciChiave(personaggioEmpty);
+		String keyDaCercare = costruisciChiave(carta);
 		
 		String path = costruisciPath("personaggi.properties");
 		
-		Set<MossaSpeciale> mosseSpeciali = ImplementazioneMosse.getMosseSpeciali();
+		Set<MossaSpeciale> mosseSpeciali = MosseSpeciali.getMosseSpeciali();
+		
 		
 		try(FileInputStream fis = new FileInputStream(path)) {
 			final Properties props = new Properties();
@@ -52,23 +62,34 @@ public class Factory implements PersonaggioFactory {
 						if(riga.startsWith(keyDaCercare)) {
 							String attributo = riga.substring(keyDaCercare.length());
 							
-							switch(attributo) {
-							case "vita":
-								personaggioEmpty.setVita(Integer.valueOf(props.getProperty(riga)));
-							case "danno":
-								personaggioEmpty.setDanno(Integer.valueOf(props.getProperty(riga)));
-							case "mossaSpeciale":
-								String nomeMossaPersonaggio = props.getProperty(riga);
+							if(carta instanceof Personaggio) {
 								
-								MossaSpeciale mossaImpl = mosseSpeciali.stream().filter(
-										mossa -> mossa.getNome().equals(nomeMossaPersonaggio)).findAny()
-													.orElseThrow(() -> new RuntimeException("error"));
+								Personaggio personaggio = (Personaggio) carta;
 								
-								personaggioEmpty.setMossaSpeciale(mossaImpl);
+								switch(attributo) {
+								case "vita":
+									personaggio.setVita(Integer.valueOf(props.getProperty(riga)));
+									break;
+								case "danno":
+									personaggio.setDanno(Integer.valueOf(props.getProperty(riga)));
+									break;
+								case "mossaSpeciale":
+									String nomeMossaPersonaggio = props.getProperty(riga);
+									
+									MossaSpeciale mossaImpl = mosseSpeciali.stream().filter(
+											mossa -> mossa.getNome().equals(nomeMossaPersonaggio)).findAny()
+														.orElseThrow(() -> new NomeNonTrovatoException("nome non presente nell'elenco"));
+									
+									
+									personaggio.setMossaSpeciale(mossaImpl);
+									break;
+								}
 							}
+
 							
 						}
 					});
+		
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -78,79 +99,34 @@ public class Factory implements PersonaggioFactory {
 			e.printStackTrace();
 		}
 		
-	}
-
-	private static <T extends Personaggio> String costruisciChiave(T personaggio) {
-		StringBuilder builder = new StringBuilder();
-		builder.append(ricercaCategoriaEimpostaNome(personaggio));
-		builder.append(puntoSeparatore);
-		builder.append(personaggio.getNome());
-		builder.append(puntoSeparatore);
 		
-		return builder.toString();
 	}
 	
-	private static <T extends Personaggio> String ricercaCategoriaEimpostaNome (T personaggioEmpty) {
+	@Override
+	public <T extends Carta> String ricercaCategoriaEimpostaNome (T carta) {
 		Enum<?>[] nomi;
-		String categoriaPersonaggio;
+		String categoriaCarta;
 		
-		if(personaggioEmpty instanceof Tank) {
+		if(carta instanceof Tank) {
 			nomi = TankNomi.values();
-			categoriaPersonaggio = Tank.class.getSimpleName();
+			categoriaCarta = Tank.class.getSimpleName();
 		
 		}
 		else
 			throw new CategoriaNonTrovataException("Categoria non ancora implementata");
 		
 		Optional<Enum<?>> optionalName = Arrays.stream(nomi)
-				.filter(n -> n.toString().equals(personaggioEmpty.getNome())).findAny();
+				.filter(n -> n.toString().equals(carta.getNome())).findAny();
 		
 		String nomeInEnum = optionalName.orElseThrow(
 				() -> new NomeNonTrovatoException("nome non presente nell'elenco")).toString();
 		
-		if(!nomeInEnum.equals(personaggioEmpty.getNome()))
-			personaggioEmpty.setNome(nomeInEnum);
-		//vuol dire che il nome inserito del personaggio è presente ed è giusto
+		if(!nomeInEnum.equals(carta.getNome()))
+			carta.setNome(nomeInEnum);
 		
-		return categoriaPersonaggio;
+		//vuol dire che il nome inserito della è presente ed è giusto
+		
+		return categoriaCarta;
 	}
-	
-	private String costruisciPath(String fileName) { 
-		String pathCompleto = getClass().getResource(cartellaConfigurazione + fileName).toExternalForm();
-		return pathCompleto.substring(pathCompleto.indexOf("C"));
-	}
-	
-	private static class ImplementazioneMosse {
-		private static Set<MossaSpeciale> mosseSpeciali = new HashSet<>();
-		
-		static {
-			MossaSpeciale mossa1 = new MossaSpeciale("ricaricaEnergia", 
-					(mossa) -> {
-						//System.out.println("hai attivato " + mossa.getNome());
-						mossa.getPersonaggioTarget().setVita(100);
-					});
-			mosseSpeciali.add(mossa1);
-		}
-		
-		private static Set<MossaSpeciale> getMosseSpeciali() {
-			return mosseSpeciali;
-		}
-	}
-	
-	private static class ImplementazionePersonaggi {
-		private static Set<Personaggio> personaggi;
-		private static PersonaggioFactory personaggioFactory = instance;
-		
-		static {
-			Tank gigante = new Tank("GIGANTE");
-			personaggioFactory.modellaPersonaggio(gigante);
-			personaggi.add(gigante);
-		}
-		
-		private static Set<Personaggio> getPersonaggi() {
-			return personaggi;
-		}
-
-	}
-		
+			
 }
