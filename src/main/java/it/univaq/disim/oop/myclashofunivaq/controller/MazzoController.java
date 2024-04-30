7de41662aac28b64e768903ec138d2d996deb838
stefanopalombo8 <stefano.palombo10@gmail.com>
2024-04-30 18:38:43 +0200

    miglioramento con l'introduzione del click sull'immagine

--- src/main/java/it/univaq/disim/oop/myclashofunivaq/controller/MazzoController.java
+++ src/main/java/it/univaq/disim/oop/myclashofunivaq/controller/MazzoController.java
@@ -2,27 +2,20 @@ package it.univaq.disim.oop.myclashofunivaq.controller;
 
 import java.net.URL;
 
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 import it.univaq.disim.oop.myclashofunivaq.business.MazzoService;
-import it.univaq.disim.oop.myclashofunivaq.business.PartitaService;
 import it.univaq.disim.oop.myclashofunivaq.business.CartaService;
 import it.univaq.disim.oop.myclashofunivaq.business.TurnoService;
 import it.univaq.disim.oop.myclashofunivaq.business.impl.Carte;
 import it.univaq.disim.oop.myclashofunivaq.business.impl.MazzoServiceImpl;
-import it.univaq.disim.oop.myclashofunivaq.business.impl.PartitaServiceImpl;
 import it.univaq.disim.oop.myclashofunivaq.business.impl.TurnoServiceImpl;
 import it.univaq.disim.oop.myclashofunivaq.controller.utilitis.GraphicUtility;
 import it.univaq.disim.oop.myclashofunivaq.domain.Carta;
 import it.univaq.disim.oop.myclashofunivaq.domain.Giocatore;
 import it.univaq.disim.oop.myclashofunivaq.domain.GiocatoreComputer;
-import it.univaq.disim.oop.myclashofunivaq.domain.GiocatoreUtente;
 import it.univaq.disim.oop.myclashofunivaq.domain.Incantesimo;
 import it.univaq.disim.oop.myclashofunivaq.domain.Mazzo;
 import it.univaq.disim.oop.myclashofunivaq.domain.Partita;
@@ -30,21 +23,13 @@ import it.univaq.disim.oop.myclashofunivaq.domain.Personaggio;
 import it.univaq.disim.oop.myclashofunivaq.view.InizializzaDati;
 import it.univaq.disim.oop.myclashofunivaq.view.ViewDispatcher;
 import it.univaq.disim.oop.myclashofunivaq.view.ViewException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Orientation;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DataFormat;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
 
@@ -65,7 +50,6 @@ public class MazzoController implements Initializable, InizializzaDati<Partita> 
 	private ViewDispatcher dispatcher;
 	private Partita partita;
 
-	private final PartitaService partitaService;
 	private final TurnoService turnoService;
 	private final CartaService cartaService;
 	private final MazzoService mazzoService;
@@ -79,7 +63,6 @@ public class MazzoController implements Initializable, InizializzaDati<Partita> 
 
 	public MazzoController() {
 		dispatcher = ViewDispatcher.getInstance();
-		partitaService = new PartitaServiceImpl();
 		cartaService = new Carte();
 		mazzoService = new MazzoServiceImpl();
 		turnoService = new TurnoServiceImpl();
@@ -108,58 +91,49 @@ public class MazzoController implements Initializable, InizializzaDati<Partita> 
 			
 			roster.getChildren().add(imageView);
 			
+			// PER I MAZZI DI TESTI
 			if(carta instanceof Personaggio)
 				personaggio = (Personaggio) carta;
 			else if(carta instanceof Incantesimo)  {
 				if(carta.getNome().equals("RendiInvulnerabile"))
 					incantesimo = (Incantesimo) carta;
 			}
-				
-			imageView.setOnDragDetected(event -> {
-				Dragboard db = imageView.startDragAndDrop(TransferMode.ANY);
-				ClipboardContent content = new ClipboardContent();
-				content.putImage(imageView.getImage());
+			
+			imageView.setOnMouseClicked(event1 -> {
+				ImageView imageViewScelta = utility.creaImpostaImageView(carta.getImmagineCarta(), dim_img, dim_img);
 				carteScelte.add(carta);
-				db.setContent(content);
-				event.consume();
-	
-			});
-			
-			mazzo.setOnDragOver(event -> {
-				if (event.getGestureSource() != mazzo && event.getDragboard().hasImage()) {
-					event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-				}
-				event.consume();
-			});
-			
-			mazzo.setOnDragDropped(event -> {
-				Dragboard db = event.getDragboard();
-				boolean success = false;
-
-				if (db.hasImage()) {
-					ImageView newImageView = utility.creaImpostaImageView(db.getImage(), dim_img, dim_img);
-					
-					utility.aggiungiCartaImmagineGriglia(mazzo, null, newImageView);
-					
-					success = true;
-
-				}
-				event.setDropCompleted(success);
-				event.consume();
+				
+				utility.aggiungiCartaImmagineGriglia(mazzo, null, imageViewScelta);
+				
+				imageViewScelta.setOnMouseClicked(event2 -> {
+					carteScelte.remove(carta);
+					mazzo.getChildren().remove(imageViewScelta);
+				});
+				
 			});
 
 		}
 		
-		for(int i = 0; i<4; i++) {
-			carteScelte.add(personaggio);
+		//MAZZI DI TEST
+		if(giocatoreCorrente instanceof GiocatoreComputer) {
+			for(int i = 0; i <8; i++) {
+				carteScelte.add(personaggio);
+			}
 		}
-		for(int i = 4; i <8; i++) {
-			carteScelte.add(incantesimo);
+		else {
+			for(int i = 0; i<4; i++) {
+				carteScelte.add(personaggio);
+			}
+			for(int i = 4; i <8; i++) {
+				carteScelte.add(incantesimo);
+			}
 		}
+		
 		
 		if(giocatoreCorrente instanceof GiocatoreComputer) {
 			this.action();
 		}
+		
 		
 	}
 
@@ -173,7 +147,8 @@ public class MazzoController implements Initializable, InizializzaDati<Partita> 
 			i++;
 			Mazzo mazzo = mazzoService.creaMazzo(carteScelte);
 			
-			// mazzoService.controllaMazzo(mazzo);
+			System.out.println(mazzoService.controllaMazzo(mazzo));
+			
 			mazzoService.aggiungiMazzo(mazzo, giocatoreCorrente);
 			
 			if (i == 1) {
